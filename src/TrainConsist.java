9445d476c4daeac10818a6de87f441cfd7cbc7a5
dakshchaudhary16 <dakshdagur953@gmail.com>
2026-03-25 14:53:19 +0530

    Base App Setup

---
+++ src/TrainConsist.java
@@ -0,0 +1,11 @@
+/**
+ * Train Consist Management App
+ * Base Application Setup
+ */
+
+public class TrainConsist {
+
+    public static void main(String[] args) {
+        System.out.println("Train Consist Management System - Base Setup");
+    }
+}
